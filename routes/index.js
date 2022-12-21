1868682b74e312673b5376ee7f9501611fb343be
Gerard Guasch <haldawe@hotmail.com>
2022-12-21 21:36:46 +0100

    working on iteration 3

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,9 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+/* GET movies page */
+router.get('/movies',  async (req, res, next) => {
+    res.status(200).render('movies')
+})
+
 module.exports = router;
