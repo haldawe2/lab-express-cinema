56ea5866378b3e90fafc4873a172319bdde2e6d8
Gerard Guasch <haldawe@hotmail.com>
2022-12-22 13:32:06 +0100

    lab done, no styling

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
 /* GET movies page */
 router.get('/movies',  async (req, res, next) => {
-    res.status(200).render('movies')
+    try {
+        const moviesFromDB = await Movie.find({})
+        res.status(200).render('movies', {moviesFromDB});
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+})
+
+/* GET movie showtimes */
+router.get('/movie/:id', async (req, res, next) => {
+    try {
+        const movieData = await Movie.findById(req.params.id);
+        res.status(200).render('movie', movieData);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 })
 
 module.exports = router;
